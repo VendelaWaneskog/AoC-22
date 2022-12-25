60c223c774d5c64e9691e8b368b24ccf99b6b9fa
Vendela Wäneskog <vendela.e.w@gmail.com>
2022-12-25 22:34:48 +0100

    First try of Day1, no solution yet

--- day1.hs
+++ day1.hs
@@ -1,2 +1,21 @@
--- Hello first commit --
--- Hello second commit --
+-- AoC 2022 DAY 1 --
+--main :: IO  -> Int
+--main = return 0
+--makeInputArray :: IO -> [[Int]]
+
+
+--list comprehentions?
+sumArrayElem :: [[Int]] -> [[Int]]
+sumArrayElem [[x]] =  [[[x]] | sum(x)]
+
+--I can use sum here!
+
+testArray :: [[Int]]
+testArray = [[1,2,3], [5,6,4], [14,3], [54], []]
+
+
+---- Notes -- 
+-- Dont really know what I am doing in haskell but I have a method to try
+-- Make the input as a txt file -> Make the txt file into a list of lists 
+-- -> Use sum on each sublist -> Extract the biggest value
+-- Possible problems is accessing list of lists and i might need to change that!
